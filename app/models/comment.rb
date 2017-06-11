b8f1f181a1fa33bfa7af18c5239be57b2aab7c77
F <839322915@qq.com>
2017-06-11 10:38:32 +0800

    add paginate for comments

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -3,6 +3,6 @@ class Comment < ApplicationRecord
   belongs_to :user
   belongs_to :article
 
-  validates :name, presence: true, length: { minimum: 2 }
+  validates :name, presence: true, length: { minimum: 2 }, uniqueness: true
   validates :description, presence: true, length: { minimum: 3 }
 end
