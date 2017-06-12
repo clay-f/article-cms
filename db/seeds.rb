0fb0daf705a7d6e5fcb0c90422632e2b5e094fb7
F <839322915@qq.com>
2017-06-12 19:30:18 +0800

    add seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Catalog.create([
+    {name: "History"},
+    {name: "Biography"},
+    {name: "Technology"}
+])
