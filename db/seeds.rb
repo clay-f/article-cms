67c3c06050f77e3a94a8d1f1fa132db31f399724
f <839322915@qq.com>
2017-11-21 20:06:54 +0800

    add html style

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,13 +1,16 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+begin
+  Catalog.delete_all
+  User.delete_all
+  Article.delete_all
 
-Catalog.create([
+  Catalog.create([
     {name: "History"},
     {name: "Biography"},
-    {name: "Technology"}
-])
+    {name: "Technology"}])
+
+  user = User.create(name: "foo", password: "123456", email: "foo@qq.com", avatar_file_name: "miss.png")
+  user.articles.create(title: "foo", body: "foo foo", catalog_id: 1)
+  user.articles.create(title: "bar", body: "bar bar", catalog_id: 2)
+rescue Exception => e
+  puts  e.message
+end
