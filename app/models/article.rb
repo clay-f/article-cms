6f69219753df40dcf35d2a905ad372ce981a85e4
f <839322915@qq.com>
2018-03-20 12:01:18 +0800

    fixed article title too short

--- app/models/article.rb
+++ app/models/article.rb
@@ -10,6 +10,6 @@ class Article < ApplicationRecord
   has_attached_file :avatar
 
   validates_attachment_content_type :avatar, content_type: /\Aimage\/.*\z/
-  validates :title, presence: true, length: { minimum: 3, maximum: 50 } , uniqueness: true
+  validates :title, presence: true, length: { minimum: 3, maximum: 100 } , uniqueness: true
   validates :body, presence: true, length: { minimum: 5 }
 end
