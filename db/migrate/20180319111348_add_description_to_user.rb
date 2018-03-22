daf0ed5b28591415e17108d4430c905698868c68
f <839322915@qq.com>
2018-03-22 23:08:45 +0800

    update user description field add default 0 val

--- db/migrate/20180319111348_add_description_to_user.rb
+++ db/migrate/20180319111348_add_description_to_user.rb
@@ -1,5 +1,5 @@
 class AddDescriptionToUser < ActiveRecord::Migration[5.1]
   def change
-    add_column :users, :description, :string
+    add_column :users, :description, :string, null: false, default: 0
   end
 end
