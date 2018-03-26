9df3620abbcf73b0cc1e4ccef5db7fdfcaee372e
f <839322915@qq.com>
2018-03-26 17:06:48 +0800

    commit comment using ajax

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -3,7 +3,7 @@ class Comment < ApplicationRecord
   belongs_to :user
   belongs_to :article
 
-  validates :description, presence: true, length: { minimum: 3 }
+  validates :description, presence: true, length: { minimum: 1 }
 
   def to_hash
     {id: self.id, name: self.name, description: self.description}
