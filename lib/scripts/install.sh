7be2896f1bf9aa9f173d0e4e7d007adb0c8a450f
f <839322915@qq.com>
2018-03-29 22:19:31 +0800

    replace echo with printf

--- lib/scripts/install.sh
+++ lib/scripts/install.sh
@@ -5,24 +5,24 @@ declare -r SYSTEM_TYPE="$(uname -s)"
 declare -r APP_PATH=`dirname $(dirname $PWD)`
 
 check_required_port() {
-    echo "start check postgresql and elasticsearch port is opening ?"
+    printf "start check postgresql and elasticsearch port is opening ?\n"
     $1 | grep -i -E 'postgresql' > /dev/null
     if [[ "$?" -ne 0 ]]; then
-        echo "postgresql not opened"
+        printf "postgresql not opened\n"
         exit 1
     fi
     $2 | grep 9200 > /dev/null
     if [[ "$?" -ne 0 ]]; then
-        echo "elasticsearch not opened"
+        printf "elasticsearch not opened\n"
         exit 1
     fi
     $3 | grep 3000 > /dev/null
     if [[ "$?" -eq 0 ]]; then
-        echo "your 3000 port is opening"
+        printf "your 3000 port is opening\n"
         printf "close it now... \n"
         kill -9 $($3 | grep 3000 | cut -c 9-13)
     fi
-    echo "check required port over"
+    printf "check required port over\n"
 }
 
 if [[ "${SYSTEM_TYPE}" =~ "Darwin" ]]; then
